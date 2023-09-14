2b018edd8cc0dfad22a0166b10896a213786e74f
dhushendar <145031194+dhushendar@users.noreply.github.com>
2023-09-14 19:36:38 +0530

    Add files via upload

---
+++ challenge 1.2.py
@@ -0,0 +1,10 @@
+year = 2023
+
+# divided by 100 means century year (ending with 00)
+# century year divided by 400 is leap year
+if (year % 400 == 0) and (year % 100 == 0):
+    print("{0} is a leap year".format(year))
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("{0} is a leap year".format(year))
+else:
+    print("{0} is not a leap year".format(year))
